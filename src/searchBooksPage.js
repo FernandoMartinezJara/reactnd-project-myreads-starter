cbb6af0607f058701fd4c4e92a4fc7f4b12874ec
Fernando Martínez <frmartinezj@achs.cl>
2021-09-21 19:26:19 -0300

    feat: Main Page
    - adding 3 shelf filter for book component
    - remove udacity notes

--- src/searchBooksPage.js
+++ src/searchBooksPage.js
@@ -10,15 +10,16 @@ class SearchBooksPage extends React.Component {
   }
 
   updateQuery = (query) => {
+
     if(query.length === 0){
       this.clearQuery();
       return;
     }
 
-    BooksAPI.search(query).then(x => 
-      {
+    BooksAPI.search(query)
+    .then(books => {
         this.setState(() => ({
-          books: x
+          books
         }))
       });
   }
@@ -42,23 +43,13 @@ class SearchBooksPage extends React.Component {
                   Close
                 </Link>
               <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
                 <input 
                   type="text" 
                   placeholder="Search by title or author" 
                   onChange= { (event) => this.updateQuery(event.target.value) }/>
-
               </div>
             </div>
             <div className="search-books-results">
-
               <ol className="books-grid">
                 { books.length > 0 ? (
                   books.map((book, i) => (
