3094aef023c2302120b1d5d1d6ba91296424237f
Fernando Martínez <frmartinezj@achs.cl>
2021-09-21 18:19:02 -0300

    feat: Search Books
    - add book component
    - add searching books with api
    - validate null input
    - remove unnecessary comments

--- src/listBooksPage.js
+++ src/listBooksPage.js
@@ -15,42 +15,42 @@ class ListBooksPage extends React.Component {
                         <h2 className="bookshelf-title">Currently Reading</h2>
                         <div className="bookshelf-books">
                             <ol className="books-grid">
-                            <li>
-                                <div className="book">
-                                <div className="book-top">
-                                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                                    <div className="book-shelf-changer">
-                                    <select>
-                                        <option value="move" disabled>Move to...</option>
-                                        <option value="currentlyReading">Currently Reading</option>
-                                        <option value="wantToRead">Want to Read</option>
-                                        <option value="read">Read</option>
-                                        <option value="none">None</option>
-                                    </select>
+                                <li>
+                                    <div className="book">
+                                    <div className="book-top">
+                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                                        <div className="book-shelf-changer">
+                                        <select>
+                                            <option value="move" disabled>Move to...</option>
+                                            <option value="currentlyReading">Currently Reading</option>
+                                            <option value="wantToRead">Want to Read</option>
+                                            <option value="read">Read</option>
+                                            <option value="none">None</option>
+                                        </select>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="book-title">To Kill a Mockingbird</div>
-                                <div className="book-authors">Harper Lee</div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="book">
-                                <div className="book-top">
-                                    <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
-                                    <div className="book-shelf-changer">
-                                    <select>
-                                        <option value="move" disabled>Move to...</option>
-                                        <option value="currentlyReading">Currently Reading</option>
-                                        <option value="wantToRead">Want to Read</option>
-                                        <option value="read">Read</option>
-                                        <option value="none">None</option>
-                                    </select>
+                                    <div className="book-title">To Kill a Mockingbird</div>
+                                    <div className="book-authors">Harper Lee</div>
                                     </div>
-                                </div>
-                                <div className="book-title">Ender's Game</div>
-                                <div className="book-authors">Orson Scott Card</div>
-                                </div>
-                            </li>
+                                </li>
+                                <li>
+                                    <div className="book">
+                                    <div className="book-top">
+                                        <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
+                                        <div className="book-shelf-changer">
+                                        <select>
+                                            <option value="move" disabled>Move to...</option>
+                                            <option value="currentlyReading">Currently Reading</option>
+                                            <option value="wantToRead">Want to Read</option>
+                                            <option value="read">Read</option>
+                                            <option value="none">None</option>
+                                        </select>
+                                        </div>
+                                    </div>
+                                    <div className="book-title">Ender's Game</div>
+                                    <div className="book-authors">Orson Scott Card</div>
+                                    </div>
+                                </li>
                             </ol>
                         </div>
                         </div>
@@ -162,12 +162,11 @@ class ListBooksPage extends React.Component {
                 </div>
 
                 <div className="open-search">
-                    <Link
+                    <Link 
                         className='open-search-button'
                         to='/search'>
                             Add a book
                     </Link>
-                    {/* <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button> */}
                 </div>
           </div>
         
--- src/searchBooksPage.js
+++ src/searchBooksPage.js
@@ -1,16 +1,46 @@
 import React from 'react';
+import Book from './book'
 import { Link } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
 
 class SearchBooksPage extends React.Component {
 
+  state = {
+    books: []
+  }
+
+  updateQuery = (query) => {
+    if(query.length === 0){
+      this.clearQuery();
+      return;
+    }
+
+    BooksAPI.search(query).then(x => 
+      {
+        this.setState(() => ({
+          books: x
+        }))
+      });
+  }
+
+  clearQuery = () => {
+    this.setState(() => ({
+      books: []
+    }))
+  }
+
     render(){
+
+      const { books } = this.state;
+
         return(
           <div className="search-books">
             <div className="search-books-bar">
               <Link 
                 className='close-search'
-                to='/'>Close</Link>
-              {/* <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button> */}
+                to='/'>
+                  Close
+                </Link>
               <div className="search-books-input-wrapper">
                 {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
@@ -20,12 +50,22 @@ class SearchBooksPage extends React.Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                <input type="text" placeholder="Search by title or author"/>
+                <input 
+                  type="text" 
+                  placeholder="Search by title or author" 
+                  onChange= { (event) => this.updateQuery(event.target.value) }/>
 
               </div>
             </div>
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+
+              <ol className="books-grid">
+                { books.length > 0 ? (
+                  books.map((book, i) => (
+                    <Book book={ book } key={ i }/>
+                  ))) : null
+                }
+              </ol>
             </div>
           </div>
         )
